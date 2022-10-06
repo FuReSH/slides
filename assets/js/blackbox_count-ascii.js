960cc5f4abedc69dc87a084647a8cf0567885d34
Till Grallert <till.grallert@fu-berlin.de>
2022-10-06 17:40:56 +0200

    modularised the Javascript

--- assets/js/blackbox_count-ascii.js
+++ assets/js/blackbox_count-ascii.js
@@ -1,31 +1,40 @@
+function preprocess(input) {
+    var output;
+    // remove non ASCII characters
+    output = removeNonAscii(input);
+    // remove all white space
+    outpu = removeWhitespace(output);
+    console.log("input: '" + input + "'");
+    console.log("output: '" + output + "'");
+    return output
+}
+
+function removeNonAscii(input) {
+    var output;
+    output = input.replace(/[^\x00-\x7F]/g, '');
+    return output
+}
+
+function removeWhitespace(input) {
+    var output;
+    output = input.replace(/ /g, '');
+    return output
+}
+
 document.getElementById("bb_button_compute").addEventListener("click", function() { 
-        // get the input string from the HTML
-        let chars = document.getElementById("bb_input").value;  
-        // pre-process the input string
-        let trimmedChars = chars.trim().replace(/ /g,'')
-        // set dummy variables for the processing and output
-        let count = 0;
-        let output_text = [];
-        // the actual computing
-        for (let index = 0; index < trimmedChars.length; index++) {
-            const element = trimmedChars[index];
-            let asc = element.charCodeAt(0);
-            if(asc < 128) {
-                output_text.push(element);
-                count ++;
-            }
-        }
+    // get the input string from the HTML
+    let input = document.getElementById("bb_input").value;
+    // pre-process the input string
+    let processedInput = preprocess(input);
+    let output = processedInput.length;
+    // write results to the HTML
+    document.getElementById("bb_result_compute").innerText = output;
+    document.getElementById("bb_input_preprocessed").innerText = processedInput;
 
-        let result_text = output_text.join("");
-        let result_count = count;
-        // write results to the HTML
-        document.getElementById("bb_result_compute").innerHTML = result_count;
-        document.getElementById("bb_input_preprocessed").innerHTML = result_text;
+});
 
-    });
+document.getElementById("bb_button_clear").addEventListener("click", function() {
 
-document.getElementById("bb_button_clear").addEventListener("click", function() { 
-    
     let text = document.getElementsByClassName('to_clear');
 
     for (let index = 0; index < text.length; index++) {
@@ -34,7 +43,7 @@ document.getElementById("bb_button_clear").addEventListener("click", function() 
             element.value = '';
         }
         if (document.getElementsByTagName("textarea")) {
-            element.innerHTML = '';
+            element.innerText = '';
         }
-    } 
+    }
 });
